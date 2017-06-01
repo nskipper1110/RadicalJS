55aacf2e27f7a2c1293d4b3789f8b7ffa24bcd00
Nathan Skipper <nathan@montgomerytechnology.com>
2017-06-01 16:50:59 -0500

    Added RadicalFrame file, changed license on Widget and added comments.

---
+++ src/widget/radical-frame.js
@@ -0,0 +1,24 @@
+/**
+ * radical-frame.js
+ * Created by Nathan Skipper, MTI
+ * Created on 6/1/2017
+ * Revision 0.1
+ * @copyright (c) 2017, Nathan Skipper
+ * License: Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ * 
+ * Description: This file contains classes related to the "frame" functionality within
+ * Radical. A frame is a graphical object which will be represented within both the Designer
+ * and the Application. The frame is a container which facilitates the layout of all widgets added
+ * to it.
+ */
